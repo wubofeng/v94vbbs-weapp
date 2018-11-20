47affaed594543e5a7e8b5edb574a041c533cfb7
wubofeng-company <wubofeng@vip.qq.com>
2018-11-20 14:27:07 +0800

    user perms

--- src/mixins/replyMixin.js
+++ src/mixins/replyMixin.js
@@ -107,7 +107,7 @@ export default class ReplyMixin extends wepy.mixin {
     if (!user) {
       return false
     }
-    return (reply.user_id === user.id)
+    return (reply.user_id === user.id) || this.$parent.can('manage_contents')
   }
   async onPullDownRefresh() {
     this.noMoreData = false
